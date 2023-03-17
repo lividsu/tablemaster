8a340f22e571b2b56ef04852c7f499f36f4e0640
Livid Su <qq1532250087@gmail.com>
2023-03-17 19:16:45 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablemaster',
-    version='0.21',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         'mysql-connector',
