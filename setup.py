6df60b3d4e894665f15fa406a003a68dd314b284
Livid Su <qq1532250087@gmail.com>
2024-11-01 14:00:42 +0800

    1.2.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablemaster',
-    version='1.2.9',
+    version='1.2.10',
     packages=find_packages(),
     install_requires=[
         'PyMySQL',
